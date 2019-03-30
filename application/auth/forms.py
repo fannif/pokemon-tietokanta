e74a5fb20a262f9ef1e1dcf5548850027b85dcf4
Fanni Franssila <fanni.franssila@gmail.com>
2019-03-30 17:15:27 +0200

    Muutettiin rekisteröitymistä niin, ettei voi ottaa sellaista käyttäjänimeä, joka on jo käytössä.

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, validators
+from wtforms import PasswordField, StringField, validators, ValidationError
+from application.auth.models import User
 
 class LoginForm(FlaskForm):
     username = StringField("Username")
@@ -11,12 +12,19 @@ class LoginForm(FlaskForm):
 class NewAccountForm(FlaskForm):
     username = StringField("Username", [validators.Length(min=4)])
     password = PasswordField("Password", [validators.Length(min=4)])
+    password_confirm = PasswordField("Confirm password", validators=[validators.DataRequired(),validators.EqualTo('password')])
 
     class Meta:
        csrf = False
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Sorry, that username is already taken.")
+
 class AccountInfoForm(FlaskForm):
     password = PasswordField("Enter new Password", [validators.Length(min=4)])
+    password_confirm = PasswordField("Confirm new password", validators=[validators.DataRequired(),validators.EqualTo('password')])
 
     class Meta:
         csrf = False
